6d3b63819f2f8286c297e8c5e733eb22b2086da2
Ubuntu <ubuntu@ip-10-248-57-36.eu-west-1.compute.internal>
2013-12-03 10:02:48 +0000

    adding productions config file

---
+++ config/autoload/production.config.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+    'db_adapter_manager' => array(
+        'xmltv' => array (
+            'catchuptv' => array(
+                'driver' => 'Pdo',
+                'dsn' => 'mysql:dbname=catchuptv;host=mm-phoenix.c1mmeweekpwh.eu-west-1.rds.amazonaws.com',
+                'username' => 'ingest_root',
+                'password' => '6fK0vgOkkIgI',
+            ),
+            'simplestreamClients' => array(
+                'driver' => 'Pdo',
+                'dsn' => 'mysql:dbname=simplestream_clients;host=mm-phoenix.c1mmeweekpwh.eu-west-1.rds.amazonaws.com',
+                'username' => 'ingest_root',
+                'password' => '6fK0vgOkkIgI',
+            ),
+        ),
+    ),
+);
